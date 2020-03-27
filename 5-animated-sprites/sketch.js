58b113e9d83207dda0946c1bd2f2ed72276f4c55
zacharylee22 <zacharyjylee@gmail.com>
2020-03-27 13:45:49 -0700

    added a face with rolling eyes

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -10,7 +10,7 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  ghost = loadAnimation('sprites/face001.png', 'sprites/face005.png');
 
 }
 
@@ -23,6 +23,6 @@ function draw() {
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
+  animation(ghost, 300, 300);
 
 }
